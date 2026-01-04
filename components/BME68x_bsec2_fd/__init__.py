c6317673f7052a428a9bab807812e0c9f7748b94
samcobb92-pixel <samcobb92@gmail.com>
2026-01-04 11:45:59 +0000

    Update __init__.py

--- components/BME68x_bsec2_fd/__init__.py
+++ components/BME68x_bsec2_fd/__init__.py
@@ -1,3 +1,20 @@
-# This file allows ESPHome to recognize this folder as a component.
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import i2c, sensor
+from esphome.const import CONF_ID
 
-# test test
+DEPENDENCIES = ["i2c"]
+AUTO_LOAD = ["bme68x_bsec2_fd"]
+
+bme690_ns = cg.esphome_ns.namespace("bme690_bsec2")
+BME690Component = bme690_ns.class_("BME690Component", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(BME690Component),
+    }
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
